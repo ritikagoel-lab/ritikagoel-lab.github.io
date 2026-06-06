510738b8284276c17910a2e36105fd8628dfeb7f
Ritika Goel <ritikagoel@Ritikas-MacBook-Air.local>
2026-06-06 16:00:05 -0700

    Update homepage content and site constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ritika Goel | Embedded Systems Leader';
+export const SITE_DESCRIPTION = 'Work and Logs of Ritika Goel.';
